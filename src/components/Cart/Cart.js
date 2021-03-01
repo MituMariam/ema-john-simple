e7c38298f8aaed66b6ea5f91f588238465374760
Mitu Mariam <mitumariam18@gmail.com>
2021-03-01 18:15:38 +0600

    total,tax,shipping cost

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,10 +3,34 @@ import React from 'react';
 const Cart = (props) => {
     const cart = props.cart
     console.log(cart);
+    const total = cart.reduce((total,prd)=> total + prd.price, 0)
+
+
+    let shipping = 0;
+    if(total > 35){
+        shipping = 0;
+    }
+    else if(total > 15){
+        shipping = 4.99;
+    }
+    else if(total > 0){
+        shipping = 12.99;
+    }
+
+
+    const tax = Math.round(total / 10);
+    // const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping +  Number(tax)).toFixed(2)
+
     return (
         <div>
             <h1>Cart</h1>
                 <h6>Items Order: {cart.length}</h6>
+                <h6>Shipping Cost: {shipping}</h6>
+                <h6>Product Price: {total}</h6>
+                <h6>tax: {tax}</h6>
+                <h6>Total Price: {grandTotal}</h6>
+                
         </div>
     );
 };
